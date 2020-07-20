f61ab0d9468fc70e3fcad108e33273d60d4d8739
szaker1990 <szaker1990@github.com>
2020-07-20 13:19:14 +0200

    Header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,24 @@ export const Header = () => {
 
     return (
         <>
-            <div className={"container row"}>
-                <div className={"col-6"}>
+            <div className={"nav__section row"}>
+                <div className={"col-6 login__image"}>
 
                 </div>
-                <div className={"col-6 login__container"}>
-                    <Link to={'/logowanie'} className="login__container-login" href="#">Zaloguj</Link>
-                    <Link to={'/rejestracja'} className="login__container-register" href="#">Załóż konto</Link>
+                <div className={"col-6"}>
+                    <div className={"login__container"}>
+                        <Link to={'/logowanie'} className="login__container-login" href="#">Zaloguj</Link>
+                        <Link to={'/rejestracja'} className="login__container-register" href="#">Załóż konto</Link>
+                    </div>
+                    <div className={"main__nav"}>
+                        <ul className={"main__nav-list"}>
+                            <li className={"main__nav-item"}>Start</li>
+                            <li className={"main__nav-item"}>O co chodzi?</li>
+                            <li className={"main__nav-item"}>O nas</li>
+                            <li className={"main__nav-item"}>Fundacja i Organizacje</li>
+                            <li className={"main__nav-item"}>Kontakt</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </>
