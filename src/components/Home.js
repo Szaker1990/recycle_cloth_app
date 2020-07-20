7b81e4afda2eeafeae2bd3f21438e800fd69ce50
szaker1990 <szaker1990@github.com>
2020-07-20 18:03:59 +0200

    Steps Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Header} from "./Header";
 import {ThreeColumns} from "./ThreeColumns";
+import {Steps} from "./Steps";
 
 export const Home = () => {
 
@@ -8,6 +9,7 @@ export const Home = () => {
         <>
             <Header/>
             <ThreeColumns/>
+            <Steps/>
         </>
     )
 }
---
+++ src/components/Steps.js
@@ -0,0 +1,50 @@
+import React from "react"
+import decoration from "../assets/decoration.svg";
+import bag from "../assets/bag.png";
+import refresh from "../assets/refresh.png";
+import scale from "../assets/scale.png";
+import shirt from "../assets/shirt.png"
+
+export const Steps = () => {
+
+    return (
+        <>
+            <div className={"row"}>
+                <div className="col-12 header__box">
+                    <h2 className="header__box-head">Wystarczą 4 proste kroki</h2>
+                </div>
+            </div>
+            <div className={"row"}>
+                <div className="col-12 img__box">
+                    <img className={"img__box-deco"} src={decoration} alt={"decoration"}/>
+                </div>
+            </div>
+            <div className={"row"}>
+                <div className="col-12 steps__box">
+                    <div className="col-8 steps__box">
+                        <div className="steps__box-card">
+                            <img className={"box__card-img"} src={shirt} alt={"shirt"}/>
+                            <h3 className="box__card-header">Wybierz rzeczy</h3>
+                            <p className="box__card-text">ubrania, zabawki,<br/> sprzęt i inne</p>
+                        </div>
+                        <div className="steps__box-card">
+                            <img className={"box__card-img"} src={bag} alt={"bag"}/>
+                            <h3 className="box__card-header">Spakuj je</h3>
+                            <p className="box__card-text">skorzystaj z <br/> worków na śmieci</p>
+                        </div>
+                        <div className="steps__box-card">
+                            <img className={"box__card-img"} src={scale} alt={"scake"}/>
+                            <h3 className="box__card-header">Zdecyduj komu chcesz pomóc</h3>
+                            <p className="box__card-text">wybierz zaufane <br/> miejsce</p>
+                        </div>
+                        <div className="steps__box-card">
+                            <img className={"box__card-img"} src={refresh} alt={"refresh"}/>
+                            <h3 className="box__card-header">Zamów kuriera</h3>
+                            <p className="box__card-text">kurier przyjedzie <br/> w dogodnym terminie</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
