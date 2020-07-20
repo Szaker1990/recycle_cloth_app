a16bc2f215c18884476db9dec0f23de026fdcdde
szaker1990 <szaker1990@github.com>
2020-07-20 15:03:00 +0200

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {Link} from "react-router-dom";
 import {Link as scrollLink ,animateScroll as scroll} from "react-scroll";
+import {decoration} from "../assets/decoration.svg"
 
 export const Header = () => {
 
@@ -8,10 +9,10 @@ export const Header = () => {
     return (
         <>
             <div className={"nav__section row"}>
-                <div className={"col-6 login__image"}>
+                <div className={"col-5 login__image"}>
 
                 </div>
-                <div className={"col-6"}>
+                <div className={"col-7"}>
                     <div className={"login__container"}>
                         <Link to={'/logowanie'} className="login__container-login" href="#">Zaloguj</Link>
                         <Link to={'/rejestracja'} className="login__container-register" href="#">Załóż konto</Link>
@@ -25,6 +26,15 @@ export const Header = () => {
                             <li className={"main__nav-item"}>Kontakt</li>
                         </ul>
                     </div>
+                    <div className={"content__box"}>
+                        <h2 className={"content__box-header"}>Zacznij pomagać!<br/> Oddaj niechciane rzeczy w zaufane ręce</h2>
+                        <img className={"content__box-img"} src={decoration} alt={"decoration"}/>
+                        <div className={"content__box-buttons"}>
+                            <Link to={'/rejestracja'} className="content__box-link" href="#"> Oddaj <br/>rzeczy</Link>
+                            <Link to={'/rejestracja'} className="content__box-link" href="#">zorganizuj<br/> zbiorke</Link>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </>
