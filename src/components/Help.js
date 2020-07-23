8b848e4f2fc809122e31f0b6f52f22543b447cd4
szaker1990 <szaker1990@github.com>
2020-07-23 16:33:53 +0200

    Style Fixes

--- src/components/Help.js
+++ src/components/Help.js
@@ -5,70 +5,60 @@ import organization from "../data/organization";
 import local from "../data/local";
 import {Paginate} from "./Paginate";
 import {PaginationButtons} from "./PaginationButtons";
-
 export const Help = () => {
-    const [helper,setHelper] = useState(foundations);
-    const [currPage,setCurrPage] = useState(1);
+    const [helper, setHelper] = useState(foundations);
+    const [currPage, setCurrPage] = useState(1);
     const [helpersPerPage] = useState(3);
     const [whoFun, setWhoFun] = useState('help__button');
     const [whoOrg, setWhoOrg] = useState('help__button');
     const [whoLoc, setWhoLoc] = useState('help__button');
-
-    const handleToFoundation = () =>{
+    const handleToFoundation = () => {
         setHelper(foundations);
         whoIsActive()
         setCurrPage(1)
     }
-
-    const handleToOrganizations = () =>{
+    const handleToOrganizations = () => {
         setHelper(organization);
         whoIsActive2()
         setCurrPage(1)
     }
-
-    const handleToLocals = () =>{
+    const handleToLocals = () => {
         setHelper(local);
         whoIsActive3()
         setCurrPage(1)
     }
-
-    const indexOfLastHelper = currPage * helpersPerPage;
-    const indexOfFirstHelper = indexOfLastHelper - helpersPerPage;
-    const currentHelpers = helper.slice(indexOfFirstHelper,indexOfLastHelper);
-
-    const paginate = (pageNumber) =>{
+    const paginate = (pageNumber) => {
         setCurrPage(pageNumber)
     }
-
-    const whoIsActive = () =>{
-        if(helper === foundations) {
+    const whoIsActive = () => {
+        if (helper === foundations) {
             setWhoFun("help__button active")
-        }else{
+        } else {
             setWhoFun("help__button")
         }
     }
-    const whoIsActive2 = () =>{
-        if(helper === organization) {
+    const whoIsActive2 = () => {
+        if (helper === organization) {
             setWhoOrg("help__button active")
-        }else{
+        } else {
             setWhoOrg("help__button")
         }
     }
-    const whoIsActive3 = () =>{
-        if(helper === local) {
+    const whoIsActive3 = () => {
+        if (helper === local) {
             setWhoLoc("help__button active")
-        }else{
+        } else {
             setWhoLoc("help__button")
         }
     }
-
-    useEffect(() =>{
+    useEffect(() => {
         whoIsActive()
         whoIsActive2()
         whoIsActive3()
-    },[handleToFoundation, handleToOrganizations,handleToLocals])
-
-
+    }, [handleToFoundation, handleToOrganizations, handleToLocals]);
+    const indexOfLastHelper = currPage * helpersPerPage;
+    const indexOfFirstHelper = indexOfLastHelper - helpersPerPage;
+    const currentHelpers = helper.slice(indexOfFirstHelper, indexOfLastHelper);
     return (
         <>
             <div id="help" className="row">
@@ -77,8 +67,8 @@ export const Help = () => {
                     <img className="help__container-img" src={decoration} alt={"decoration"}/>
                     <div className="help__container-buttons">
                         <button className={whoFun} onClick={handleToFoundation}>Fundacjom</button>
-                        <button className={whoOrg}  onClick={handleToOrganizations}>Organizacjom pozarządowym</button>
-                        <button className={whoLoc}  onClick={handleToLocals}>Lokalny zbiórkom</button>
+                        <button className={whoOrg} onClick={handleToOrganizations}>Organizacjom pozarządowym</button>
+                        <button className={whoLoc} onClick={handleToLocals}>Lokalny zbiórkom</button>
                     </div>
                     <p className="help__container-text">W naszej bazie znajdziesz listę
                         zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się
@@ -87,9 +77,9 @@ export const Help = () => {
                 </div>
             </div>
             <div className="row help__setup">
-                    <Paginate items={currentHelpers}/>
-                    <PaginationButtons postsPerPage={helpersPerPage} totalPosts={helper.length} paginate={paginate}/>
-                </div>
+                <Paginate items={currentHelpers}/>
+                <PaginationButtons postsPerPage={helpersPerPage} totalPosts={helper.length} paginate={paginate}/>
+            </div>
         </>
     )
 }
