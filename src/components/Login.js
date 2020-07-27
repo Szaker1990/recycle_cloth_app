753ffd3bd4fe911bf8e4504ccc03c109629e9d2c
szaker1990 <szaker1990@github.com>
2020-07-27 14:48:20 +0200

    Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import {Link as ScrollLink} from "react-scroll";
 import decoration from "../assets/decoration.svg";
 
 export const Login = () => {
+    const [user,setUser] = useState({
+        email: "",
+        password: "",
+    });
+    const[emailError,setEmailError] = useState("");
+    const[passwordError,setPasswordError] = useState("")
 
+    const handleChangeUserData = e => {
+        const {name, value} = e.target;
+        setUser(prev => ({
+                ...prev,
+                [name]: value
+            })
+        );
+    }
+    const handleLogin = (e) => {
+        e.preventDefault()
+        if (user.email < 5 && !user.email.includes("@")) {
+            setEmailError("Podany email jest nieprawidłowy!");
+        } else {
+            setEmailError("");
+        }
+        if (user.password < 6) {
+            setPasswordError("Podane hasło jest za krótkie!");
+        } else {
+            setPasswordError("");
+
+        }
+    }
     return (
         <>
             <div className="row">
@@ -29,16 +57,20 @@ export const Login = () => {
                 <div className={"col-12 login"}>
                     <h2 className={"login__header"}>Zaloguj się</h2>
                     <img className={"form__img"} src={decoration} alt={"decoration"}/>
-                    <form className={"login__form"}>
-                        <div>
-                            <label className={"login__form-label"}>Email</label>
-                            <input type={"text"} className={"login__form-email"}/>
-                            <label className={"login__form-label"}>Hasło</label>
-                            <input type={"text"} className={"login__form-password"}/>
+                    <form onSubmit={handleLogin} className={"login__form"}>
+                        <div className={"login__input-wrapper"}>
+                            <div className={"wrapper__handler"}>
+                                <label className={"login__form-label"}>Email</label>
+                                <input type={"text"} name={"email"} onChange={handleChangeUserData} value={user.email} className={"login__form-email"}/>
+                                <p className={"error__message"}>{emailError}</p>
+                                <label className={"login__form-label"}>Hasło</label>
+                                <input  type={"text"} name={"password"}  onChange={handleChangeUserData} value={user.password} className={"login__form-password"}/>
+                                <p className={"error__message"}>{passwordError}</p>
+                            </div>
                         </div>
-                        <div>
+                        <div className={"login__button-wrapper"}>
                             <button className={"btn__register"}>Załóż konto</button>
-                            <input className={"btn__login-submit"} type={"submit"} value={"Zaloguj sie"}/>
+                            <input className={"active"} type={"submit"} value={"Zaloguj sie"}/>
                         </div>
                     </form>
                 </div>
