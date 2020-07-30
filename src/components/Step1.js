77063c9822bb97b712bb38c26548f139cca56bda
szaker1990 <szaker1990@github.com>
2020-07-30 10:35:02 +0200

    step 1 style done

--- src/components/Step1.js
+++ src/components/Step1.js
@@ -2,34 +2,6 @@ import React from "react"
 
 export const Step1 = () => {
 
-    const checkboxes = [
-        {
-            name: 'Ubrania, które nadają sie do ponownego użycia',
-            key: '1',
-            label: 'Ubrania, które nadają sie do ponownego użycia',
-        },
-        {
-            name: 'ubrania, do wyrzucenia',
-            key: '2',
-            label: 'ubrania, do wyrzucenia',
-        },
-        {
-            name: 'zabawki',
-            key: '3',
-            label: 'zabawki',
-        },
-        {
-            name: 'książki',
-            key: '4',
-            label: 'książki',
-        },
-        {
-            name: 'inne',
-            key: '5',
-            label: 'Inne',
-        },
-    ];
-
     return (
         <>
             <div className={"row yellow__box"}>
@@ -45,20 +17,32 @@ export const Step1 = () => {
                     <h3 className={"bear__box-step"}>Krok 1/4</h3>
                     <h2 className={"bear__box-header"}>Zaznacz co chcesz oddać:</h2>
                     <form className={"bear__box-form"}>
-                        {
-                            checkboxes.map(box => (
-                                <>
-                                    <div className={"checkbox__wrapper"}>
-                                        <input className={"checkbox__input"} type={"checkbox"} value={box.name}/>
-                                        <label className={"checkbox__label"} key={box.key}> {box.name}</label>
-                                    </div>
-                                </>
-                            ))
-                        }
+                        <div className={"bear__box-wrapper"}>
+                            <input className={"radio__input"} type={"radio"} value={"ubrania, które nadaja sie do ponownego użycia"} name={"giveaway"} />
+                            <label className={"radio__label"}>ubrania, które nadaja sie do ponownego użycia</label>
+                        </div>
+                        <div className={"bear__box-wrapper"}>
+                            <input className={"radio__input"} type={"radio"} value={"ubrania, do wyrzucenia"} name={"giveaway"} />
+                            <label className={"radio__label"}>ubrania, do wyrzucenia</label>
+                        </div>
+                        <div className={"bear__box-wrapper"}>
+                            <input className={"radio__input"} type={"radio"} value={"zabawki"} name={"giveaway"} />
+                            <label className={"radio__label"}>zabawki</label>
+                        </div>
+                        <div className={"bear__box-wrapper"}>
+                            <input className={"radio__input"} type={"radio"} value={"książki"} name={"giveaway"} />
+                            <label className={"radio__label"}>książki</label>
+                        </div>
+                        <div className={"bear__box-wrapper"}>
+                            <input className={"radio__input"} type={"radio"} value={"inne"} name={"giveaway"} />
+                            <label className={"radio__label"}>inne</label>
+                        </div>
+                        <div className={"bear__box-buttons"}>
+                            <button className={"bear__box-btn"}>Dalej</button>
+                        </div>
                     </form>
                 </div>
             </div>
         </>
     )
-
 }
