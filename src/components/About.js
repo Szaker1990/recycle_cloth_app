7ea23a79f47816552dab8b0beec4adf10b33b245
szaker1990 <szaker1990@github.com>
2020-08-17 11:19:48 +0200

    rwd columns about

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,8 @@ import signature from "../assets/Signature.svg";
 export const About = () => {
     return (
         <>
-            <div id="about" className={"row section__about"}>
-                <div className={"col-6 about__container"}>
+            <div id={"about"} className={"row section__about"}>
+                <div id={"about__wrapper"} className={"col-6 about__container"}>
                     <h2 className={"about__header"}>O nas</h2>
                     <img className={"about__decoration"} src={decoration} alt={"decoration"}/>
                     <p className={"about__text"}>Nori grape silver beet broccoli kombu beet greens fava bean potato
@@ -14,7 +14,7 @@ export const About = () => {
                     </p>
                     <img className={"about__signature"} src={signature} alt={"signature"}/>
                 </div>
-                <div className="col-6 about__picture"></div>
+                <div className={"col-6 about__picture"}></div>
             </div>
         </>
     )
--- src/components/Steps.js
+++ src/components/Steps.js
@@ -13,37 +13,37 @@ export const Steps = () => {
     return (
         <>
             <div className={"row"} id="steps">
-                <div className="col-12 header__box">
-                    <h2 className="header__box-head">Wystarczą 4 proste kroki</h2>
+                <div className={"col-12 header__box"}>
+                    <h2 className={"header__box-head"}>Wystarczą 4 proste kroki</h2>
                 </div>
             </div>
             <div className={"row"}>
-                <div className="col-12 img__box">
+                <div className={"col-12 img__box"}>
                     <img className={"img__box-deco"} src={decoration} alt={"decoration"}/>
                 </div>
             </div>
             <div className={"row"}>
-                <div className="col-12 steps__box">
-                    <div className="col-8 steps__box">
-                        <div className="steps__box-card">
+                <div className={"col-12 steps__box"}>
+                    <div className={"col-8 steps__box"} id={"step__wrapper"}>
+                        <div className={"steps__box-card"}>
                             <img className={"box__card-img"} src={shirt} alt={"shirt"}/>
-                            <h3 className="box__card-header">Wybierz rzeczy</h3>
-                            <p className="box__card-text">ubrania, zabawki,<br/> sprzęt i inne</p>
+                            <h3 className={"box__card-header"}>Wybierz rzeczy</h3>
+                            <p className={"box__card-text"}>ubrania, zabawki,<br/> sprzęt i inne</p>
                         </div>
-                        <div className="steps__box-card">
+                        <div className={"steps__box-card"}>
                             <img className={"box__card-img"} src={bag} alt={"bag"}/>
-                            <h3 className="box__card-header">Spakuj je</h3>
-                            <p className="box__card-text">skorzystaj z <br/> worków na śmieci</p>
+                            <h3 className={"box__card-header"}>Spakuj je</h3>
+                            <p className={"box__card-text"}>skorzystaj z <br/> worków na śmieci</p>
                         </div>
-                        <div className="steps__box-card">
+                        <div className={"steps__box-card"}>
                             <img className={"box__card-img"} src={scale} alt={"scake"}/>
-                            <h3 className="box__card-header">Zdecyduj komu chcesz pomóc</h3>
-                            <p className="box__card-text">wybierz zaufane <br/> miejsce</p>
+                            <h3 className={"box__card-header"}>Zdecyduj komu chcesz pomóc</h3>
+                            <p className={"box__card-text"}>wybierz zaufane <br/> miejsce</p>
                         </div>
-                        <div className="steps__box-card">
+                        <div className={"steps__box-card"}>
                             <img className={"box__card-img"} src={refresh} alt={"refresh"}/>
-                            <h3 className="box__card-header">Zamów kuriera</h3>
-                            <p className="box__card-text">kurier przyjedzie <br/> w dogodnym terminie</p>
+                            <h3 className={"box__card-header"}>Zamów kuriera</h3>
+                            <p className={"box__card-text"}>kurier przyjedzie <br/> w dogodnym terminie</p>
                         </div>
                     </div>
                 </div>
