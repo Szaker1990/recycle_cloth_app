17905c57917f6931fe4f7906184fcc29a7812b1c
szaker1990 <szaker1990@github.com>
2020-08-20 10:08:09 +0200

    Updates

---
+++ precache-manifest.5bcb262bb9f3663cb4a210fcf3d091a1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c5f2a872c6220c020e8d89500769a6",
+    "url": "/recycle_cloth_app/index.html"
+  },
+  {
+    "revision": "fc20426f473ee851a8c8",
+    "url": "/recycle_cloth_app/static/css/main.750b5523.chunk.css"
+  },
+  {
+    "revision": "1ef6d8c9b0143160ae99",
+    "url": "/recycle_cloth_app/static/js/2.74fa6568.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/recycle_cloth_app/static/js/2.74fa6568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc20426f473ee851a8c8",
+    "url": "/recycle_cloth_app/static/js/main.44073a1e.chunk.js"
+  },
+  {
+    "revision": "42b4e87c2ee2abd149ef",
+    "url": "/recycle_cloth_app/static/js/runtime-main.b8a252e4.js"
+  },
+  {
+    "revision": "80a70b6f49e0aa104ea9400badd00572",
+    "url": "/recycle_cloth_app/static/media/People.80a70b6f.png"
+  },
+  {
+    "revision": "d87cadf24b263287f4d33024c61c02e5",
+    "url": "/recycle_cloth_app/static/media/Signature.d87cadf2.svg"
+  },
+  {
+    "revision": "28770ab2f4bfac963e4bd94740f238be",
+    "url": "/recycle_cloth_app/static/media/background.28770ab2.png"
+  },
+  {
+    "revision": "bfe6cd18b7e4dd9222cf021655f5b53b",
+    "url": "/recycle_cloth_app/static/media/bear.bfe6cd18.png"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/recycle_cloth_app/static/media/columns_bg.99f2e620.png"
+  },
+  {
+    "revision": "76b47d5049797ef506d5a242168a14a4",
+    "url": "/recycle_cloth_app/static/media/header__img.76b47d50.png"
+  },
+  {
+    "revision": "6baf5950179f12f109e487a7ec03faac",
+    "url": "/recycle_cloth_app/static/media/shirt.6baf5950.svg"
+  },
+  {
+    "revision": "87008d4ca9bf5d7f61c34875b5ff53ad",
+    "url": "/recycle_cloth_app/static/media/sweater.87008d4c.png"
+  }
+]);
